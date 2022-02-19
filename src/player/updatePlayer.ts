e2738bea9ee6639f80ab47731ded5767f67606fd
Oliver Carlsson <oliver@framecore.se>
2022-02-19 19:54:54 +0100

    Parameter type bugfix

--- src/player/updatePlayer.ts
+++ src/player/updatePlayer.ts
@@ -1,21 +1,21 @@
+import {
+  playerOnFloor,
+  setPlayerOnFloor,
+  playerVelocity,
+  playerCollider,
+  camera
+} from './player';
+import { gravity } from '../input/commands/settingsHandler';
+import { worldOctree } from '../scene/mapLoader';
 
-import { playerOnFloor, setPlayerOnFloor, playerVelocity, playerCollider, camera } from "./player";
-import { gravity } from "../input/commands/settingsHandler";
-import { worldOctree } from "../scene/mapLoader";
-
-
-export const updatePlayer = (deltaTime) => {
-
-  let damping = Math.exp(- 20 * deltaTime) - 1;
-
+export const updatePlayer = (deltaTime: number) => {
+  let damping = Math.exp(-20 * deltaTime) - 1;
 
   if (!playerOnFloor) {
-
     playerVelocity.y -= gravity * deltaTime;
 
     // small air resistance
     damping *= 0.2;
-
   }
 
   playerVelocity.addScaledVector(playerVelocity, damping);
@@ -26,27 +26,23 @@ export const updatePlayer = (deltaTime) => {
   playerCollisions();
 
   camera.position.copy(playerCollider.end);
-
-}
+};
 
 function playerCollisions() {
-
   const result = worldOctree.capsuleIntersect(playerCollider);
 
-  setPlayerOnFloor(false)
+  setPlayerOnFloor(false);
 
   if (result) {
-
-    setPlayerOnFloor(result.normal.y > 0)
+    setPlayerOnFloor(result.normal.y > 0);
 
     if (!playerOnFloor) {
-
-      playerVelocity.addScaledVector(result.normal, - result.normal.dot(playerVelocity));
-
+      playerVelocity.addScaledVector(
+        result.normal,
+        -result.normal.dot(playerVelocity)
+      );
     }
 
     playerCollider.translate(result.normal.multiplyScalar(result.depth));
-
   }
-
 }
